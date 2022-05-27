963f034a229d2aacb20eb4b1cff5fc3c530d3d07
Kholish Nurfajri <kholisnurfajri1@gmail.com>
2022-05-27 15:21:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+PORT = 5000;
+const isLogin = false;
+
+app.set("view engine","hbs");
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/",(req, res) =>{
+    res.render("index")
+});
+
+app.get("/project/:index",(req, res) =>{
+    const index = req.params.index
+    console.log(index)
+
+    res.render("detail-project", {index : index})
+
+});
+
+app.get("/contact",(req, res) =>{
+    res.render("contact-form")
+});
+
+app.get("/addproject",(req, res) =>{
+    res.render("add-project")
+
+});
+
+app.post("/addproject",(req, res) =>{
+
+const data = req.body
+
+console.log(data)
+
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server on PORT:${PORT}`)
+})
